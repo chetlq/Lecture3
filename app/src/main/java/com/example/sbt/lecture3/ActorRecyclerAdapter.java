7bbf53f91575429777dfc0652cde1ea670b1a79e
Your Name <you@example.com>
2018-10-27 22:47:33 +0300

    начало домашки

--- app/src/main/java/com/example/sbt/lecture3/ActorRecyclerAdapter.java
+++ app/src/main/java/com/example/sbt/lecture3/ActorRecyclerAdapter.java
@@ -2,6 +2,7 @@ package com.example.sbt.lecture3;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
@@ -20,11 +23,20 @@ public class ActorRecyclerAdapter extends RecyclerView.Adapter<ActorRecyclerAdap
     private final LayoutInflater inflater;
     @NonNull
     private final Context context;
+    @NonNull
+    private final RequestManager imageLoader;
 
     public ActorRecyclerAdapter(@NonNull List<Actor> actors,  @NonNull Context context) {
         this.actors = actors;
         this.inflater = LayoutInflater.from(context);
         this.context = context;
+
+        RequestOptions imageOption = new RequestOptions()
+                .placeholder(R.drawable.ic_android_black_24dp)
+                .fallback(R.drawable.ic_android_black_24dp)
+                .centerCrop();
+        this.imageLoader = Glide.with(context).applyDefaultRequestOptions(imageOption);
+
     }
 
 
@@ -36,10 +48,11 @@ public class ActorRecyclerAdapter extends RecyclerView.Adapter<ActorRecyclerAdap
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Actor actor = actors.get(position);
-        holder.nameView.setText(actor.getName());
-        Glide.with(context).load(actor.getAvatar()).into(holder.avatarView);
-        holder.oscarView.setVisibility(actor.isHasOscar()? View.VISIBLE : View.GONE);
+
+        holder.bind( actors.get(position));
+//        holder.nameView.setText(actor.getName());
+//        Glide.with(context).load(actor.getAvatar()).into(holder.avatarView);
+//        holder.oscarView.setVisibility(actor.isHasOscar()? View.VISIBLE : View.GONE);
     }
 
     @Override
@@ -53,7 +66,7 @@ public class ActorRecyclerAdapter extends RecyclerView.Adapter<ActorRecyclerAdap
     }
 
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder {
         private final ImageView avatarView;
         private final TextView nameView;
         private final ImageView oscarView;
@@ -65,11 +78,37 @@ public class ActorRecyclerAdapter extends RecyclerView.Adapter<ActorRecyclerAdap
             oscarView = itemView.findViewById(R.id.oscar);
         }
 
-//        void bind(Actor actor) {
-//            imageLoader.load(actor.getAvatarUrl()).into(avatarView);
-//            nameView.setText(actor.getName());
-//            oscarView.setVisibility(actor.hasOscar() ? View.VISIBLE : View.GONE);
-//        }
+        void bind(Actor actor) {
+            imageLoader.load(actor.getAvatar()).into(avatarView);
+            nameView.setText(actor.getName());
+            oscarView.setVisibility(actor.isHasOscar() ? View.VISIBLE : View.GONE);
+        }
     }
+
+//    public void replaceData(List<Actor> newActor){
+//        DiffUtil.DiffResult diff = DiffUtil.calculateDiff(
+//                new DiffUtil.Callback() {
+//                    @Override
+//                    public int getOldListSize() {
+//                        return 0;
+//                    }
+//
+//                    @Override
+//                    public int getNewListSize() {
+//                        return 0;
+//                    }
+//
+//                    @Override
+//                    public boolean areItemsTheSame(int i, int i1) {
+//                        return false;
+//                    }
+//
+//                    @Override
+//                    public boolean areContentsTheSame(int i, int i1) {
+//                        return false;
+//                    }
+//                }
+//        )
+//    }
 
 }
--- app/src/main/java/com/example/sbt/lecture3/MainActivity.java
+++ app/src/main/java/com/example/sbt/lecture3/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.sbt.lecture3;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -31,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
 //        ListView listView = findViewById(R.id.list_view);
 //        listView.setAdapter(new ActorListAdapter(DataUtils.generateActors(),this));
 //        listView.setOnItemClickListener(clickListener);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_switch:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
